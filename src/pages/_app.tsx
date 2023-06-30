5231b42da640ad13cff977cf0a01a4751a84e4fd
kangod <kangod.aka@gmail.com>
2023-06-30 19:53:34 +0800

    1、导航栏功能完成，提交第一版

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
@@ -11,6 +12,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      { process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false}/> }
     </SessionProvider>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,12 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    }
   },
   plugins: [],
 } satisfies Config;
